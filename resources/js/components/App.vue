d2af49114fe72895b88031db62bd65beedfae4df
sandesh <sandeshsays@gmail.com>
2020-09-08 13:02:50 +0545

    panels

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="h-screen flex items-center justify-center">
-    <div class="w-32 h-32 bg-blue-500 rounded-full"></div>
-  </div>
+  <section class="m-16">
+    <h1 class="text-5xl font-semibold mb-8">Panels</h1>
+
+    <base-panel class="mb-8">
+    Sint dolor ad anim laborum sunt. Eu voluptate voluptate nostrud culpa. Aliquip fugiat reprehenderit incididunt magna. Consequat do velit dolore pariatur officia duis. Elit commodo ut minim consectetur veniam elit deserunt duis nulla cupidatat ex occaecat ad. Et ut ipsum sint elit fugiat commodo officia occaecat consequat fugiat laborum aute id. Ut adipisicing ullamco aute est laborum irure velit adipisicing voluptate est.
+    </base-panel>
+
+    <base-panel class="mb-8 flex" noPadding>
+    <div class="bg-blue-500 w-64"></div>
+    <div class="px-6 py-4 flex-1">
+        In elit amet incididunt quis officia nostrud quis voluptate labore dolor id. Cillum sint do nulla ut occaecat reprehenderit culpa. Commodo cillum in fugiat ipsum fugiat enim aliquip id occaecat est excepteur. Commodo esse sint officia cupidatat amet. Proident minim duis esse mollit sunt ullamco officia qui occaecat deserunt.
+    </div>
+    </base-panel>
+
+    <base-panel class="mb-8" title="My Title" sub="Some metadata">
+    Ipsum mollit excepteur nostrud eu. Voluptate ex nulla aute velit officia reprehenderit id ipsum excepteur eu. Ea dolor ullamco ad deserunt.
+    </base-panel>
+
+  </section>
 </template>
 
 <script>
+import BasePanel from "./BasePanel";
 export default {
-  //
+  components: { BasePanel },
 };
 </script>
---
+++ resources/js/components/BasePanel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-white rounded-lg shadow-md overflow-hidden" :class="{'px-6 py-4':! noPadding}">
+      <div class="mb-8" v-if="title">
+        <h1 class="text-grey-900 text-xl" v-text="title"></h1>
+        <div class="text-sm text-grey-600 uppercase tracking-wider font-semibold mt-1" v-text="sub"></div>
+      </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    noPadding: Boolean,
+    title:String,
+    sub: String,
+  }
+}
+</script>
+
+<style>
+</style>
