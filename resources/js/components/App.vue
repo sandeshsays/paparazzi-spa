b6e459e56e68732ef4ee4d227d76ea18c86910b3
sandesh <sandeshsays@gmail.com>
2020-09-08 10:19:20 +0545

    tailwind css
    set up tailwind css

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>Content here ok...</div>
+  <div class="h-screen flex items-center justify-center">
+    <div class="w-32 h-32 bg-blue-500 rounded-full"></div>
+  </div>
 </template>
 
 <script>
